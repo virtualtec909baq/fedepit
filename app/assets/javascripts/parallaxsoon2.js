d0298106d9d3bc3d9d773d117eb56f3de67348cb
jquiu <jenniffer54@gmail.com>
2015-05-20 14:49:15 -0500

    add first commit

---
+++ app/assets/javascripts/parallaxsoon2.js
@@ -0,0 +1,137 @@
+﻿var WIDTH;
+var HEIGHT;
+var canvas;
+var con;
+var g;
+var pxs = new Array();
+var rint = 50;
+var numberObject = 2000;
+var numberBall = 40;
+var pulsion = 1;
+var radius = 38;
+var speedX = 5;
+var speedY = 2;
+
+
+function BrowserDetection() {
+
+    if (/Firefox[\/\s](\d+\.\d+)/.test(navigator.userAgent)) {
+
+        var ffversion = new Number(RegExp.$1);
+        rint = 10;
+        pulsion = 3;
+    }
+
+    else if (/MSIE (\d+\.\d+);/.test(navigator.userAgent)) {
+
+        var ieversion = new Number(RegExp.$1);
+        rint = 50;
+        pulsion = 16;
+    }
+
+    else if (/Chrome[\/\s](\d+\.\d+)/.test(navigator.userAgent)) {
+        var chromeversion = new Number(RegExp.$1);
+        rint = 50;
+        pulsion = 16;
+
+    }
+    else if (/Opera[\/\s](\d+\.\d+)/.test(navigator.userAgent)) {
+
+        var oprversion = new Number(RegExp.$1)
+        rint = 50;
+        pulsion = 16;
+    }
+    else if (/Safari[\/\s](\d+\.\d+)/.test(navigator.userAgent)) {
+        var safariversion = new Number(RegExp.$1);
+        rint = 50;
+        pulsion = 16;
+
+    }
+    WIDTH = window.innerWidth;
+
+    if (WIDTH < 1024) {
+        numberObject = 19000;
+        numberBall = 10;
+        pulsion = 2;
+        rint = 10;
+    }
+}
+
+$(document).ready(function () {
+    BrowserDetection();
+    
+    WIDTH = window.innerWidth;
+    HEIGHT = window.innerHeight;
+    canvas = document.getElementById('pixie');
+    $(canvas).attr('width', WIDTH).attr('height', 450);
+    con = canvas.getContext('2d');
+    for (var i = 0; i < numberBall; i++) {
+        pxs[i] = new Circle();
+        pxs[i].reset();
+    }
+    setInterval(draw, rint);
+
+});
+
+function draw() {
+    con.clearRect(0, 0, WIDTH, HEIGHT);
+    for (var i = 0; i < pxs.length; i++) {
+        pxs[i].fade();
+        pxs[i].move();
+        pxs[i].draw();
+    }
+}
+
+function Circle() {
+    WIDTH = window.innerWidth;
+    HEIGHT = window.innerHeight;
+    this.s = { ttl: numberObject, xmax: speedX, ymax: speedY, rmax: radius, rt: pulsion, xdef: 960, ydef: 540, xdrift: 4, ydrift: 4, random: true, blink: true };
+
+    this.reset = function () {
+        this.x = (this.s.random ? WIDTH * Math.random() : this.s.xdef);
+        this.y = (this.s.random ? HEIGHT * Math.random() : this.s.ydef);
+        this.r = ((this.s.rmax - 1) * Math.random()) + 1;
+        this.dx = (Math.random() * this.s.xmax) * (Math.random() < .5 ? -1 : 1);
+        this.dy = (Math.random() * this.s.ymax) * (Math.random() < .5 ? -1 : 1);
+        this.hl = (this.s.ttl / rint) * (this.r / this.s.rmax);
+        this.rt = Math.random() * this.hl;
+        this.s.rt = Math.random() + 1;
+        this.stop = Math.random() * .2 + .4;
+        this.s.xdrift *= Math.random() * (Math.random() < .5 ? -1 : 1);
+        this.s.ydrift *= Math.random() * (Math.random() < .5 ? -1 : 1);
+    }
+
+    this.fade = function () {
+        this.rt += this.s.rt;
+    }
+
+    this.draw = function () {
+        if (this.s.blink && (this.rt <= 0 || this.rt >= this.hl)) this.s.rt = this.s.rt * -1;
+        else if (this.rt >= this.hl) this.reset();
+        var newo = 1 - (this.rt / this.hl);
+        con.beginPath();
+        con.arc(this.x, this.y, this.r, 0, Math.PI * 2, true);
+        con.closePath();
+        var cr = this.r * newo;
+        g = con.createRadialGradient(this.x, this.y, 0, this.x, this.y, (cr <= 0 ? 1 : cr));
+        g.addColorStop(0.0, 'rgba(47,236,236,' + newo + ')');
+        g.addColorStop(this.stop, 'rgba(13,193,193,' + (newo * .3) + ')');
+        g.addColorStop(1.0, 'rgba(255,255,255,0)');
+        con.fillStyle = g;
+        con.fill();
+    }
+
+    this.move = function () {
+        WIDTH = window.innerWidth;
+        HEIGHT = window.innerHeight;
+        this.x += (this.rt / this.hl) * this.dx;
+        this.y += (this.rt / this.hl) * this.dy;
+        if (this.x > WIDTH || this.x < 0) this.dx *= -1;
+        if (this.y > HEIGHT || this.y < 0) this.dy *= -1;
+    }
+
+    this.getX = function () { return this.x; }
+    this.getY = function () { return this.y; }
+}
+
+
---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,257 @@
+﻿
+$(document).ready(function ($) {
+    /*************************************************************
+    Preload
+    *************************************************************/
+    $(function preview() {
+        "use strict";
+        try {
+            var myTimer = 0; clearTimeout(myTimer);
+            myTimer = setTimeout(function () { $("#loader-wrapper").slideUp() }, 2000);
+
+        } catch (err) {
+        }
+    });
+
+
+    /*************************************************************
+    Percentage skillbar
+    *************************************************************/
+    $(function skillbar() {
+        "use strict";
+        try {
+            $('.skillbar').each(function () {
+                $(this).find('.skillbar-bar').animate({
+                    width: $(this).attr('data-percent')
+                }, 6000);
+            });
+        } catch (err) {
+        }
+    });
+
+    /*************************************************************
+    Linebar
+    *************************************************************/
+    function lineBar() {
+        "use strict";
+        try {
+            $('.linebar').each(function () {
+                $(this).find('.linebar-bar').animate({
+                    width: $(this).attr('data-percent')
+                }, 4000);
+            });
+        } catch (err) {
+        }
+    };
+
+    /*************************************************************
+    Tooltip
+    *************************************************************/
+    $(function tooltip() {
+        "use strict";
+        $('[data-toggle="tooltip"]').tooltip()
+    });
+
+   
+    /*************************************************************
+    Filter Porfolio
+    *************************************************************/
+    $(function filter() {
+        "use strict";
+        try {
+            $('.filter li').on('click', function () {
+                $('li').removeClass('active');
+                $(this).addClass('active');
+            });
+        } catch (err) {
+        }
+    });
+
+    /*************************************************************
+    Filter Porfolio Dresses
+    *************************************************************/
+    $(function filter() {
+        "use strict";
+        try {
+            $('.filter-dresses li').on('click', function () {
+                $('li').removeClass('active');
+                $(this).addClass('active');
+            });
+        } catch (err) {
+        }
+    });
+
+    /*************************************************************
+    Carousel
+    *************************************************************/
+    $(function () {
+        "use strict";
+        $("#owl-demo").owlCarousel({
+            navigation: false, // Show next and prev buttons
+            slideSpeed: 300,
+            paginationSpeed: 400,
+            singleItem: true,
+            autoPlay: true
+            // "singleItem:true" is a shortcut for:
+            // items : 1, 
+            // itemsDesktop : false,
+            // itemsDesktopSmall : false,
+            // itemsTablet: false,
+            // itemsMobile : false
+
+        });
+    });
+
+    /*************************************************************
+    Small FlexSlider With thumbs
+    *************************************************************/
+    $(function flexThumb() {
+        "use strict";
+        $('.flexthumb').flexslider({
+            animation: "slide",
+            controlNav: "thumbnails"
+
+        });
+    });
+
+    /*************************************************************
+    Small FlexSlider Slide
+    *************************************************************/
+    $(function flexImg() {
+        "use strict";
+        $('.flexslider').flexslider({
+            animation: "slide",
+            controlNav: ""
+
+        });
+    });
+
+    /*************************************************************
+    FancyBox Open Image
+    *************************************************************/
+    $(function fancyImg() {
+        "use strict";
+        try {
+            $(".fancybox-effects-d").fancybox({
+                padding: 0,
+
+                openEffect: 'elastic',
+                openSpeed: 150,
+
+                closeEffect: 'elastic',
+                closeSpeed: 150,
+
+                closeClick: true,
+
+                helpers: {
+                    css: {
+                        'background': 'rgba(238,238,238,0.85)'
+                    },
+                    overlay: {
+                        locked: false
+                    }
+                }
+            });
+        } catch (err) {
+        }
+    });
+
+
+    /**
+    * Copyright (c) 2007-2012 Ariel Flesler - aflesler(at)gmail(dot)com | http://flesler.blogspot.com
+    * Dual licensed under MIT and GPL.
+    * @author Ariel Flesler
+    * @version 1.4.3
+    */
+    ; (function ($) { var h = $.scrollTo = function (a, b, c) { $(window).scrollTo(a, b, c) }; h.defaults = { axis: 'xy', duration: parseFloat($.fn.jquery) >= 1.3 ? 0 : 1, limit: true }; h.window = function (a) { return $(window)._scrollable() }; $.fn._scrollable = function () { return this.map(function () { var a = this, isWin = !a.nodeName || $.inArray(a.nodeName.toLowerCase(), ['iframe', '#document', 'html', 'body']) != -1; if (!isWin) return a; var b = (a.contentWindow || a).document || a.ownerDocument || a; return /webkit/i.test(navigator.userAgent) || b.compatMode == 'BackCompat' ? b.body : b.documentElement }) }; $.fn.scrollTo = function (e, f, g) { if (typeof f == 'object') { g = f; f = 0 } if (typeof g == 'function') g = { onAfter: g }; if (e == 'max') e = 9e9; g = $.extend({}, h.defaults, g); f = f || g.duration; g.queue = g.queue && g.axis.length > 1; if (g.queue) f /= 2; g.offset = both(g.offset); g.over = both(g.over); return this._scrollable().each(function () { if (!e) return; var d = this, $elem = $(d), targ = e, toff, attr = {}, win = $elem.is('html,body'); switch (typeof targ) { case 'number': case 'string': if (/^([+-]=)?\d+(\.\d+)?(px|%)?$/.test(targ)) { targ = both(targ); break } targ = $(targ, this); if (!targ.length) return; case 'object': if (targ.is || targ.style) toff = (targ = $(targ)).offset() } $.each(g.axis.split(''), function (i, a) { var b = a == 'x' ? 'Left' : 'Top', pos = b.toLowerCase(), key = 'scroll' + b, old = d[key], max = h.max(d, a); if (toff) { attr[key] = toff[pos] + (win ? 0 : old - $elem.offset()[pos]); if (g.margin) { attr[key] -= parseInt(targ.css('margin' + b), 10) || 0; attr[key] -= parseInt(targ.css('border' + b + 'Width'), 10) || 0 } attr[key] += g.offset[pos] || 0; if (g.over[pos]) attr[key] += targ[a == 'x' ? 'width' : 'height']() * g.over[pos] } else { var c = targ[pos]; attr[key] = c.slice && c.slice(-1) == '%' ? parseFloat(c) / 100 * max : c } if (g.limit && /^\d+$/.test(attr[key])) attr[key] = attr[key] <= 0 ? 0 : Math.min(attr[key], max); if (!i && g.queue) { if (old != attr[key]) animate(g.onAfterFirst); delete attr[key] } }); animate(g.onAfter); function animate(a) { $elem.animate(attr, f, g.easing, a && function () { a.call(this, e, g) }) } }).end() }; h.max = function (a, b) { var c = b == 'x' ? 'Width' : 'Height', scroll = 'scroll' + c; if (!$(a).is('html,body')) return a[scroll] - $(a)[c.toLowerCase()](); var d = 'client' + c, html = a.ownerDocument.documentElement, body = a.ownerDocument.body; return Math.max(html[scroll], body[scroll]) - Math.min(html[d], body[d]) }; function both(a) { return typeof a == 'object' ? a : { top: a, left: a} } })(jQuery);
+
+
+    $(document).on("scroll", onScroll);
+
+    function onScroll(event) {
+        try {
+            var scrollPos = $(window).scrollTop();
+
+            //Page Index
+            if ($('#about4ourteam').length > 0) {
+                if (scrollPos > $('#about4ourteam').offset().top) {
+                    lineBar();
+                }
+            }
+
+            //Page index
+            if ($('#cbp-vm').length > 0) {
+                if (scrollPos > $('#cbp-vm').offset().top) {
+                    $('.img-trasp').addClass('fadeInLeftPhone');
+
+                    var myTimerPhone = 0; clearTimeout(myTimerPhone);
+
+                    myTimerPhone = setTimeout(function () { $('.iphone-desc ul li:nth-child(1)').addClass('fadeInDown') }, 600);
+                    myTimerPhone = setTimeout(function () { $('.iphone-desc ul li:nth-child(2)').addClass('fadeInDown') }, 900);
+                    myTimerPhone = setTimeout(function () { $('.iphone-desc ul li:nth-child(3)').addClass('fadeInDown') }, 1200);
+                    myTimerPhone = setTimeout(function () { $('.iphone-desc ul li:nth-child(4)').addClass('fadeInDown') }, 1500);
+                    myTimerPhone = setTimeout(function () { $('.iphone-desc ul li:nth-child(5)').addClass('fadeInDown') }, 1800);
+                }
+            }
+
+            if ($('#home2Pass').length > 0) {
+                if (scrollPos > $('#home2Pass').offset().top) {
+                    var myTimerPhone = 0; clearTimeout(myTimerPhone);
+
+                    myTimerPhone = setTimeout(function () { $('.iphone-desc ul li:nth-child(1)').addClass('fadeInDown') }, 600);
+                    myTimerPhone = setTimeout(function () { $('.iphone-desc ul li:nth-child(2)').addClass('fadeInDown') }, 900);
+                    myTimerPhone = setTimeout(function () { $('.iphone-desc ul li:nth-child(3)').addClass('fadeInDown') }, 1200);
+                    myTimerPhone = setTimeout(function () { $('.iphone-desc ul li:nth-child(4)').addClass('fadeInDown') }, 1500);
+                    myTimerPhone = setTimeout(function () { $('.iphone-desc ul li:nth-child(5)').addClass('fadeInDown') }, 1800);
+                }
+            }
+
+            //Page about4
+            if ($('#html5service').length > 0) {
+                if (scrollPos > $('#html5service').offset().top) {
+                    $('.effect-diagonal').addClass('effectopacity');
+                }
+            }
+
+
+        } catch (err) {
+            //            console.log('error',err);
+        }
+    }
+
+});
+/*************************************************************
+    Porfolio
+*************************************************************/
+$(window).load(function () {
+   try {
+    var $container = $('.albumContainer');
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
+    });
+
+    $('.albumFilter li').on('click', function () {
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+        });
+        return false;
+    });
+    } catch (err) {
+    }
+});
+
+
+
+
+
