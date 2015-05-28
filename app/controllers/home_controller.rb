ca5baba4f2e562a360b1f10d03d0192ca930fc20
alexander0915 <alez.0915@gmail.com>
2015-05-28 09:46:29 -0500

    modificacion index metodo que muestra los patrocinadores

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,9 @@
 class HomeController < ApplicationController
   def index
-  	@sponsors = Sponsor.all
+  	@sponsors = Sponsor.last(5).reverse
   	@articles = Article.last(3).reverse	
   	@canines = Canine.last(4).reverse
+
   end
   def contactus
   end
