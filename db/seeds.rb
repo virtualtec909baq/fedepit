133f9e20fa05347e64fb8f09dcae05c2cd3025a9
Jenniffer Quintero <jenniffer54@Jenniffers-MacBook-Pro.local>
2015-05-29 15:16:01 -0400

    add new seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,9 +10,21 @@ if Video.all.empty?
 	end
 end
 
+if Race.all.empty?
+	10.times  do 
+		Race.create(name: Faker::Name.name, description: Faker::Lorem.paragraph(3))
+	end
+end
+
 if TypeEvent.all.empty?
 	30.times do
 		TypeEvent.create(name: Faker::Name.title)
+	end
+end
+
+if Sponsor.all.empty?
+	10.times do
+		Sponsor.create(name: Faker::Name.title, url: Faker::Internet.url, image: Faker::Avatar.image)
 	end
 end
 
@@ -27,6 +39,7 @@ if Breeder.all.empty?
 		Breeder.create(name: Faker::Name.name, type_breeder_id: rand(1..29), phone: Faker::Number.number(10), email: Faker::Internet.email, name_breed: Faker::Company.name, facebook: Faker::Internet.url, twitter: Faker::Internet.url, instagram: Faker::Internet.url, web: Faker::Internet.url)
 	end
 end
+
 if Color.all.empty?
 	30.times do
 		Color.create(name: Faker::Commerce.color)
