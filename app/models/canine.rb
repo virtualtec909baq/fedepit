0eb203902637e6d5a47c9472ad79507f97b526b1
Jenniffer Quintero <jenniffer54@Jenniffers-MacBook-Pro.local>
2015-05-29 16:22:34 -0400

    remove validate

--- app/models/canine.rb
+++ app/models/canine.rb
@@ -8,16 +8,16 @@ class Canine < ActiveRecord::Base
  	
   self.per_page = 15
 	# validates
-	validates :race_id, presence: true 
-	validates :breeder_id, presence: true 
-	validates :lof, presence: true 
-	validates :chip, presence: true 
-	validates :name, presence: true 
-	validates :gender, presence: true 
-	validates :color_id, presence: true 
-	validates :father_lof, presence: true 
-	validates :mother_lof, presence: true 
-	validates :rate, presence: true 
-	validates :birth, presence: true 
-	validates :death, presence: true 
+	# validates :race_id, presence: true 
+	# validates :breeder_id, presence: true 
+	# validates :lof, presence: true 
+	# validates :chip, presence: true 
+	# validates :name, presence: true 
+	# validates :gender, presence: true 
+	# validates :color_id, presence: true 
+	# validates :father_lof, presence: true 
+	# validates :mother_lof, presence: true 
+	# validates :rate, presence: true 
+	# validates :birth, presence: true 
+	# validates :death, presence: true 
 end
