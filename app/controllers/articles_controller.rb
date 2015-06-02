e8649922e5efe90848dba2342231b4284f74a607
Jenniffer Quintero <jenniffer54@Jenniffers-MacBook-Pro.local>
2015-06-02 10:41:19 -0400

    paginate articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.all.order(created_at: :desc)
+    @articles = Article.all.order(created_at: :desc).paginate(page: params[:page], per_page: 4)
   end
   def show
   	@article = Article.find(params[:id])
