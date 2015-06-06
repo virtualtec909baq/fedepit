6930a4728be2834374909bc56e7fcfa461b414c8
Jenniffer Quintero <jenniffer54@Jenniffers-MacBook-Pro.local>
2015-06-06 13:58:21 -0400

    add pedigree view

--- app/controllers/canines_controller.rb
+++ app/controllers/canines_controller.rb
@@ -2,8 +2,17 @@ class CaninesController < ApplicationController
 
   # GET /canines
   # GET /canines.json
+  # GET /canines
+  # GET /canines.json
   def index
     @canines = Canine.all
+    @search = Canine.ransack(params[:q])
+    @canines = @search.result.order(created_at: :desc).page(params[:page])
+    @canine = Canine.new
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
   
 end
