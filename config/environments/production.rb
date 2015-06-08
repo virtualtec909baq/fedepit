4734443ec7dae963198539f6a2e4d526a51d2d5a
alexander0915 <alez.0915@gmail.com>
2015-06-08 15:55:05 -0500

    new deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
   config.log_level = :debug
-  config.serve_static_assets = true
+  config.serve_static_files  = true
   config.assets.compile = true
   config.i18n.fallbacks = true
 
