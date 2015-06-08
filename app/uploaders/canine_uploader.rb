28858a0bac19031893a0e09bed6fbb3583238c99
alexander0915 <alez.0915@gmail.com>
2015-06-08 16:02:37 -0500

    rm RMagick

--- app/uploaders/canine_uploader.rb
+++ app/uploaders/canine_uploader.rb
@@ -3,7 +3,7 @@
 class CanineUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
-  include CarrierWave::RMagick
+  # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
@@ -13,12 +13,12 @@ class CanineUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  version :thumb do
-    process :resize_to_limit => [200, 200]
-  end
-  version :index do
-    process :resize_to_limit => [400, 400]
-  end
+  # version :thumb do
+  #   process :resize_to_limit => [200, 200]
+  # end
+  # version :index do
+  #   process :resize_to_limit => [400, 400]
+  # end
   
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
