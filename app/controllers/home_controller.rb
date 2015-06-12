30903ec7579e68ce09be0d8e22135d3f35ea69a8
alexander0915 <alez.0915@gmail.com>
2015-06-12 10:55:42 -0500

    Mostrar patrocinadores activos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-  	@sponsors = Sponsor.last(5).reverse
+  	@sponsors = Sponsor.where(status: true).last(5).reverse
   	@articles = Article.last(3).reverse	
   	@canines = Canine.last(4).reverse
   end
