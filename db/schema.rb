de8d238aa1769596d36595bb4744b88fc73f2b5a
alexander0915 <alez.0915@gmail.com>
2015-06-12 10:57:48 -0500

    creacion de campo email en sponsors

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150607180350) do
+ActiveRecord::Schema.define(version: 20150612151317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -194,6 +194,7 @@ ActiveRecord::Schema.define(version: 20150607180350) do
     t.datetime "updated_at", null: false
     t.string   "name"
     t.string   "url"
+    t.string   "email"
   end
 
   create_table "type_breeders", force: :cascade do |t|
