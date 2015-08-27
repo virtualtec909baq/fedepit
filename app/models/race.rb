8bee2f4630ba97ce2124bb28fe8e0d9d9a2ffbef
jquiu <jenniffer54@gmail.com>
2015-08-27 17:35:13 -0500

    fix error

--- app/models/race.rb
+++ app/models/race.rb
@@ -1,6 +1,6 @@
 class Race < ActiveRecord::Base
 	has_many :canines, :dependent => :nullify
-	has_many img_races, :dependent => :destroy
+	has_many :img_races, :dependent => :destroy
 	
 	# default for will_paginate
   	self.per_page = 15
