0004574596d511d78242050b56e3a0dbc6cf4fef
jquiu <jenniffer54@gmail.com>
2015-09-08 15:04:46 -0500

    set new route to deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,6 +8,7 @@ set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
 
 server '166.62.85.175', user: 'fedepit', roles: %w{web app db}, primary: true
 
+sudo chown deploy:deploy /var/www/
 set :deploy_to, "/var/www/fedepit"
 
 # dont try and infer something as important as environment from
