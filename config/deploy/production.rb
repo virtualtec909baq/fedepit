d6bb3b65ad2b0ff0efcc3ee554b5171a978d4bfc
jquiu <jenniffer54@gmail.com>
2015-09-10 17:58:31 -0500

    change path deploy production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,7 +8,7 @@ set :full_app_name, "#{fetch(:application)}_#{fetch(:stage)}"
 
 server '166.62.89.40', user: 'fedepit', roles: %w{web app db}, primary: true
 
-set :deploy_to, "/var/www/fedepit"
+set :deploy_to, "/var/www/html/fedepit"
 
 # dont try and infer something as important as environment from
 # stage name.
