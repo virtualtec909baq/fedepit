fb3d2ad7d10eac05be39d1fc1aba869583e9799d
jquiu <jenniffer54@gmail.com>
2015-10-02 14:17:34 -0500

    validate videos

--- app/models/video.rb
+++ app/models/video.rb
@@ -5,12 +5,12 @@ class Video < ActiveRecord::Base
 	# default for will_paginate
   	self.per_page = 15
   	# validates
-	validates :title, presence: true 
+	  validates :title, presence: true 
   	validates :description, presence: true 
   	validates :url, presence: true 
   	validates :type_category_id, presence: true 
   	validates_length_of :description, :maximum => 2000
-	validates_length_of :description, :minimum => 200
+	  validates_length_of :description, :minimum => 200
 
   	belongs_to :type_category
 
