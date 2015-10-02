b96797cbe68cf89928a45577e8d33b6a92707197
jquiu <jenniffer54@gmail.com>
2015-10-02 17:18:29 -0500

    paragraph

--- app/models/paragraph.rb
+++ app/models/paragraph.rb
@@ -3,8 +3,7 @@ class Paragraph < ActiveRecord::Base
 	tracked owner: ->(controller, model) { controller && controller.current_user }
 	# validates
 	validates :description, presence: true
-	validates_length_of :description, :maximum => 2000
-    belongs_to :article
+	belongs_to :article
 	# img_uploader
   	mount_uploader :img, ArticleUploader
 end
