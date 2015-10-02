d4d08cc26f78a00ad0c7383e3b3643d264ce1311
jquiu <jenniffer54@gmail.com>
2015-10-02 11:46:39 -0500

    add validate type breeders

--- app/models/breeder.rb
+++ app/models/breeder.rb
@@ -4,6 +4,7 @@ class Breeder < ActiveRecord::Base
 	belongs_to :type_breeder
 	has_many :canines, :dependent => :nullify
 	validates :type_breeder_id, presence: true 
+	validates :name, presence: true 
 	# img_uploader
 	mount_uploader :img, BreederUploader
 end
