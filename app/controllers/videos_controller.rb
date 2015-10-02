a2b546be992f9df59a5aedadc7448c93040f705e
jquiu <jenniffer54@gmail.com>
2015-10-02 10:36:50 -0500

    add video list

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -4,10 +4,11 @@ class VideosController < ApplicationController
   # GET /videos.json
   def index
   	@search = Video.ransack(params[:q])
-    @videos = @search.result.order(created_at: :desc).paginate(page: params[:page], per_page: 4)
+    @videos = @search.result.order(title: :desc).paginate(page: params[:page], per_page: 10)
   end
   
   def show
   	@video = Video.find(params[:id])
+  	@videos = Video.last(4).reverse	
   end
 end
