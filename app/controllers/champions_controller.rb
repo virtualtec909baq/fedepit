3f799b14d65bfddae187eb84d15c2373d12ba2ee
jquiu <jenniffer54@gmail.com>
2015-11-14 10:20:37 -0500

    filterschampions

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -7,7 +7,6 @@ class ChampionsController < ApplicationController
     @champions = Champion.all
     @search = Champion.ransack(params[:q])
     @champions = @search.result.order(created_at: :ASC).paginate(page: params[:page], per_page: 12)
-    
   end
 
   # GET /champions/1
