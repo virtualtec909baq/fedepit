6df55d430062034cbb6d2311c2d643c388a3383d
jquiu <jenniffer54@gmail.com>
2015-11-25 11:41:27 -0500

    default_scope

--- app/models/characteristic.rb
+++ app/models/characteristic.rb
@@ -1,5 +1,6 @@
 class Characteristic < ActiveRecord::Base
 	acts_as_list
+  	default_scope order('position')
 	include PublicActivity::Model
 	tracked owner: ->(controller, model) { controller && controller.current_user }
 	has_many :standards
