1926330ef5ec9c871cdc52618cdffa312bf29e6f
jquiu <jenniffer54@gmail.com>
2015-11-25 11:45:15 -0500

    rm acts_as_list

--- app/models/characteristic.rb
+++ app/models/characteristic.rb
@@ -1,6 +1,4 @@
 class Characteristic < ActiveRecord::Base
-	acts_as_list
-  	default_scope order('position')
 	include PublicActivity::Model
 	tracked owner: ->(controller, model) { controller && controller.current_user }
 	has_many :standards
