7c63f5e54255f85098322d7c38a7b094e136c9d6
jquiu <jenniffer54@gmail.com>
2015-12-10 10:32:40 -0500

    image canines uploader

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,24 +1,25 @@
 # encoding: utf-8
-
 class ImageUploader < CarrierWave::Uploader::Base
-
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-
-  # Choose what kind of storage to use for this uploader:
+  include CarrierWave::MiniMagick
   storage :file
   
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # version :thumb do
-  #   process :resize_to_fit => [108, 81]
-  # end
+  version :main do
+    process resize_to_fit: [800, 900]
+  end
 
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
+  version :thumb do
+    process resize_to_fill: [200,200]
+  end
+  version :thumb_2 do
+    process resize_to_fill: [250,250]
+  end
+
   def extension_white_list
     %w(jpg jpeg gif png)
   end
